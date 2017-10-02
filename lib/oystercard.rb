1bb3d71dc828d6b576c9ea07e2ce8b04dd7ac2e3
Chris <cdunham1989@gmail.com>
2017-10-02 17:55:32 +0100

    max balance set, unit tests written to ensure error raised

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,17 @@
 class Oystercard
   attr_reader :balance
 
-  def initialize(balance = 0)
+  MAX_BALANCE = 90
+
+  def initialize(balance = 0, max_balance = MAX_BALANCE)
     @balance = balance
+    @max_balance = max_balance
   end
 
   def top_up(amount)
+    raise "Max balance of #{:max_balance} exceeded!" if balance + amount > @max_balance
     @balance += amount
+    #@balance = @balance + amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,17 +1,24 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
 
   it 'a new card has a balance of 0' do
-    expect(subject.balance).to eq(0)
+    expect(oystercard.balance).to eq(0)
   end
 
   it 'can be topped up' do
-     expect(subject).to respond_to(:top_up).with(1).argument
+     expect(oystercard).to respond_to(:top_up).with(1).argument
   end
 
   it 'changes balance when topped up' do
-    expect{ subject.top_up 2 }.to change{ subject.balance }.by 2
+    expect{ oystercard.top_up 2 }.to change{ oystercard.balance }.by 2
+  end
+
+  it 'prevents balance exceeding 90' do
+    max_balance = Oystercard::MAX_BALANCE
+    oystercard.top_up(max_balance)
+    expect {oystercard.top_up(4)}.to raise_error "Max balance of #{:max_balance} exceeded!"
   end
 
 end
