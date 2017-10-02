958be17e1b371697a1923b3f1637c64bae6d4bc5
Chris <cdunham1989@gmail.com>
2017-10-02 16:43:47 +0100

    top_up method written, unit test passed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,8 +6,12 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
-  it 'can be topped up to change balance' do
+  it 'can be topped up' do
      expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  it 'changes balance when topped up' do
+    expect{ subject.top_up 2 }.to change{ subject.balance }.by 2
   end
 
 end
